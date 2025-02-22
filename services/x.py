369da1469df24a1b8b5fe31153d1fa3e73a215b4
ねんねこ <nennneko5787@gmail.com>
2025-02-22 11:33:41 +0900

    x.py を更新

--- services/x.py
+++ services/x.py
@@ -1,20 +1,15 @@
 import os
 
-import twikit
+from twikit.guest import GuestClient
 import dotenv
 
 dotenv.load_dotenv()
 
 
 class XService:
-    client: twikit.Client = None
+    client: GuestClient = None
 
     @classmethod
     async def login(cls):
-        cls.client = twikit.Client(language="ja-JP")
-        await cls.client.login(
-            auth_info_1=os.getenv("x_account_id"),
-            auth_info_2=os.getenv("x_account_email"),
-            password=os.getenv("x_account_password"),
-            cookies_file="cookie.json",
-        )
+        cls.client = GuestClient()
+        await cls.client.activate()
